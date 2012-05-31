705299e9f45dadfa9afaa2baa3c075666b68f0e9
Chris Cyvas <chris.cyvas@gmail.com>
2012-05-30 20:32:02 -0400

    starting dc v4.0

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.5.000.*")]
+[assembly: AssemblyVersion("3.5.0.*")]
--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Mettle Systems LLC")]
-[assembly: AssemblyCopyright("© 2007-2009 Mettle System LLC.  All rights reserved.")]
+[assembly: AssemblyCopyright("© 2007-20012 Mettle System LLC.  All rights reserved.")]
 [assembly: AssemblyTrademark("dashCommerce™")]
 [assembly: AssemblyProduct("dashCommerce™")]
 
