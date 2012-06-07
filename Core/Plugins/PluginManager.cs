d573416947a1b062cb9b2debfc42e8a512924e4e
Chris Cyvas <chris.cyvas@gmail.com>
2012-06-07 14:18:27 -0400

    starting the plugins implementation

---
+++ Core/Plugins/PluginManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Web;
+
+using MettleSystems.dashCommerce.Core.Plugins;
+
+[assembly: PreApplicationStartMethod(typeof(PluginManager), "Initialize")]
+namespace MettleSystems.dashCommerce.Core.Plugins {
+  
+  public class PluginManager {
+
+    private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim();
+
+    public static void Initialize() {
+      using (new WriteLockDisposable(Locker)) { 
+        
+      }
+    }
+
+  }
+}
---
+++ Core/WriteLockDisposable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace MettleSystems.dashCommerce.Core {
+  /// <summary>
+  /// Provides a convenience methodology for implementing locked access to resources. 
+  /// </summary>
+  /// <remarks>
+  /// Intended as an infrastructure class.
+  /// </remarks>
+  public class WriteLockDisposable : IDisposable {
+    
+    private readonly ReaderWriterLockSlim _rwLock;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WriteLockDisposable"/> class.
+    /// </summary>
+    /// <param name="rwLock">The rw lock.</param>
+    public WriteLockDisposable(ReaderWriterLockSlim rwLock) {
+      _rwLock = rwLock;
+      _rwLock.EnterWriteLock();
+    }
+
+    void IDisposable.Dispose() {
+      _rwLock.ExitWriteLock();
+    }
+  }
+}
