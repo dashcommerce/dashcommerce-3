8752fede449547b4a70b7fa74b0ce73ab9ad0fc0
Chris Cyvas <chris.cyvas@gmail.com>
2012-06-12 11:46:39 -0400

    plugins

--- Core/Plugins/PluginManager.cs
+++ Core/Plugins/PluginManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Web;
-
+using System.Web.Hosting;
 using MettleSystems.dashCommerce.Core.Plugins;
 
 [assembly: PreApplicationStartMethod(typeof(PluginManager), "Initialize")]
@@ -10,10 +12,35 @@ namespace MettleSystems.dashCommerce.Core.Plugins {
   public class PluginManager {
 
     private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim();
+    private static DirectoryInfo shadowCopyFolder;
+    private static bool? isFullTrust;
+    private static DirectoryInfo pluginFolder;
 
     public static void Initialize() {
-      using (new WriteLockDisposable(Locker)) { 
-        
+      using (new WriteLockDisposable(Locker)) {
+        var pluginPath = HostingEnvironment.MapPath("~/App_Plugins");
+        pluginFolder = new DirectoryInfo(pluginPath);
+
+        /* 
+         * 
+         * may need to add in shadowcopy stuff here
+         * 
+         */
+
+        var pluginFiles = Enumerable.Empty<FileInfo>();
+        try {
+          pluginFiles = pluginFolder.GetFiles("*.dll", SearchOption.AllDirectories)
+            .ToArray()
+            .Where(x => x.Directory.Parent != null)
+            .ToList();
+
+          Console.WriteLine(pluginFiles.ToString());
+          
+        }
+        catch (Exception ex) {
+          Logger.LogException("Failed to load plugin", ex, Logger.LogMessageType.Error);
+        }
+
       }
     }
 
